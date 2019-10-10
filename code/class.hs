6893e62226fa35bbaff486d88c278563524ee49f
Denis Maggi <maggidenis@gmail.com>
2019-10-10 14:28:06 +0200

    Class 20191010

--- code/class.hs
+++ code/class.hs
@@ -1,5 +1,10 @@
--- 09/10/2019
 import Test.QuickCheck
+
+--------------------------------
+--------------------------------
+-- 09/10/2019
+--------------------------------
+--------------------------------
 
 
 factorial :: Integer -> Integer
@@ -39,3 +44,56 @@ raices a b c
 square :: Integer -> Integer
 square x = x*x
 p1 x y = True ==> square x + square y+2*x*y == square(x+y)
+
+
+--------------------------------
+--------------------------------
+-- 10/10/2019
+--------------------------------
+--------------------------------
+
+--Ejemplo de recursión de cola
+resto :: Integer -> Integer -> Integer
+resto x y 
+    | x < y = x
+    | otherwise = resto (x-y) y
+
+    --Esto n oes una recursión de cola
+cociente :: Integer -> Integer -> Integer
+cociente x y
+    | x < y = 0 
+    | otherwise = 1 + cociente (x-y) y
+
+cocienteYresto :: Integer -> Integer -> (Integer,Integer)
+cocienteYresto x y
+    | otherwise = (cociente x y, resto x y)
+
+cocienteYresto' :: Integer -> Integer -> (Integer,Integer)
+cocienteYresto' x y
+    | x < y = (0,x)
+    | otherwise = (u+1,v)
+        where (u,v) = cocienteYresto (x-y) y
+
+--Función que cuente el número de ceros que tiene un numero al final
+cerosDe :: Integer -> Integer
+cerosDe x
+    | x == 0    = 1                 --Este es el caso base, sin el, se queda en bucle infinito
+--  | x < 10    = 0                 --Esta se puede omitir, no es necesaria
+    | r == 0    = 1 + cerosDe c     --Si la division me da restor 0 es que es divisible por 10, asi que tiene un 0, luego lo sumo
+    | otherwise = 0                 --Por ejemplo si es 75, no seguios buscando, porque no es ninguno de los casos anteriores
+        where (c,r) = divMod x 10
+
+--Ejercicio 1
+
+--a)
+esTerna :: Integer -> Integer -> Integer -> Bool
+esTerna x y z = x*x + y*y == z*z    --Se coloca asi por una cuestion de eficiencia, si usamos ^ se pasara a un tipo Double
+
+--b)
+terna :: Integer -> Integer -> (Integer,Integer,Integer)
+terna x y | x > y = (x*x - y*y, 2*x*y, x*x+y*y)
+
+--c)
+p_ternas :: Integer -> Integer -> Property
+p_ternas x y = x>0 && y>0 && x>y ==> esTerna a b c
+    where (a,b,c) = terna x y
